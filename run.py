e6d44624153899c552290f7eab81ea70f1922d01
maryclaire teahan <maryclaireteahan@gmail.com>
2023-09-18 13:24:09 +0100

    doc: update function

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def get_sales_data():
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas")
-    print("Example: 10, 20, 30, 40, 50, 60")
+    print("Example: 10, 20, 30, 40, 50, 60\n")
 
     data_str = input("Enter your data here:")
     print(f"The dara provided is {data_str}")
