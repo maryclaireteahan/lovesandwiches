d9261997c8881f683308ef3a6552dd2519282bed
maryclaire teahan <maryclaireteahan@gmail.com>
2023-09-19 01:21:39 +0100

    REFACTOR FUNCTIONS

--- run.py
+++ run.py
@@ -56,10 +56,15 @@ def validate_data(values):
     return True
 
 
+"""
+CODE
 def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
+"""
+"""    
+COMMENT Update sales worksheet, add new row with the list data provided
+"""    
+"""
+CODE
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
@@ -67,14 +72,26 @@ def update_sales_worksheet(data):
 
 
 def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
+"""
+"""   
+ COMMENT Update surplus worksheet, add new row with the list data provided
+"""   
+"""
+CODE
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
-
+"""
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    update the relevant worksheet with the data provided 
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updates sucessfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -95,9 +112,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
